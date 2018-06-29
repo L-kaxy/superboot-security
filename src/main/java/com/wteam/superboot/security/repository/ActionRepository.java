b29247ae430636116b97c7f4be310f59ee44ae5b
DESKTOP-2DC3SS2\L-kaxy <770619071@qq.com>
2018-06-29 11:14:50 +0800

    修正版本

--- src/main/java/com/wteam/superboot/security/repository/ActionRepository.java
+++ src/main/java/com/wteam/superboot/security/repository/ActionRepository.java
@@ -11,7 +11,7 @@ import com.wteam.superboot.security.entity.po.ActionPo;
  * 接口 Repository.
  * 
  * @author 罗佳欣
- * @version 1.0.0
+ * @version 1.2.0
  */
 public interface ActionRepository extends SuperRepository<ActionPo, Long> {
 
